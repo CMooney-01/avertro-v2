d6ad4fc06c89afafe17e331963b186839547b8f7
unknown <chris.mooney@blackflagbarbell.com.au>
2022-02-15 17:22:14 +1100

    More styling updates. Have included google font Poppins and made some general css updates. Next up - basic responsiveness.

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -5,7 +5,7 @@ function SideNav() {
   return(
 
     <div className="navbar">
-      <a className="nav-item" href="#section"><FontAwesomeIcon icon="chart-column" size="2x"/>Reports</a>
+      <a className="nav-item active" href="#section"><FontAwesomeIcon icon="chart-column" size="2x"/>Reports</a>
       <a className="nav-item" href="#section"><FontAwesomeIcon icon="wrench" size="2x"/>Strategy</a>
       <a className="nav-item" href="#section"><FontAwesomeIcon icon="stethoscope" size="2x"/>Assessments</a>
       <a className="nav-item" href="#section"><FontAwesomeIcon icon="book" size="2x"/>Catalogue</a>
