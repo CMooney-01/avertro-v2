eeb03002cb68ed96ded2023a5ef27c70abd1d075
unknown <chris.mooney@blackflagbarbell.com.au>
2022-02-15 13:18:48 +1100

    Moved reusable components over from spaghetti project. Looking promising! Just need to be able to create more (independant) form components, and control displaying key measures.

--- src/components/StrategicObjectiveForm.js
+++ src/components/StrategicObjectiveForm.js
@@ -4,38 +4,61 @@ import {useFormik} from 'formik';
 
 const FormComponent = () => {
 
+      let today = new Date();
+      let defaultStartDate = today.toLocaleDateString('en-CA');
+
+      //formatting end date to be 1 month later by default
+      let year = today.getFullYear();
+      let month = `${today.getMonth() + 2}`.padStart(2, "0");
+      let day = `${today.getDate()}`.padStart(2, "0");
+
+      let defaultEndDate = [year, month, day].join("-");
+
       const formik = useFormik({
         initialValues:{
           objective: "",
           measure1: "",
           measure2: "",
-          measure3: ""
+          measure3: "",
+          startDate: defaultStartDate,
+          endDate: defaultEndDate
         },
         onSubmit: values => {
-          alert(JSON.stringify(values));
+          localStorage.setItem("formData", JSON.stringify(values));
+          alert("Updated!");
         }
       });
 
       return (
-        <div>
-          <form onSubmit={formik.handleSubmit}>
-            <p>
-              <label htmlFor="objective">Objective: </label>
-              <input type="text" name="objective" value={formik.values.objective} onChange={formik.handleChange}></input>
-            </p>
-            <p>
-              <label htmlFor="measure1">Key Measure 1: </label>
-              <input type="text" name="measure1" value={formik.values.measure1} onChange={formik.handleChange}></input>
-            </p>
-            <p>
-              <label htmlFor="measure2">Key Measure 2: </label>
-              <input type="text" name="measure2" value={formik.values.measure2} onChange={formik.handleChange}></input>
-            </p>
-            <p>
-              <label htmlFor="measure3">Key Measure 3: </label>
-              <input type="text" name="measure3" value={formik.values.measure3} onChange={formik.handleChange}></input>
-            </p>
-            <button type="submit">Update</button>
+        <div className="stratObjContainer">
+          <form onSubmit={formik.handleSubmit} className="formContainer">
+            <div className="form-left">
+              <p>
+                <label htmlFor="objective">Objective: </label>
+                <input type="text" name="objective" value={formik.values.objective} onChange={formik.handleChange} className="form-input" size="90"></input>
+              </p>
+              <p>
+                <label htmlFor="measure1">Key Measures: </label>
+                <input type="text" name="measure1" value={formik.values.measure1} onChange={formik.handleChange} className="form-input" size="90"></input>
+              </p>
+              <p>
+                <input type="text" name="measure2" value={formik.values.measure2} onChange={formik.handleChange} className="form-input" size="90"></input>
+              </p>
+              <p>
+                <input type="text" name="measure3" value={formik.values.measure3} onChange={formik.handleChange} className="form-input" size="90"></input>
+              </p>
+              <button type="submit" className="formUpdateButton">Update</button>
+            </div>
+            <div className="form-right">
+              <p>
+                <label htmlFor="startDate">Start Date: </label>
+                <input type="date" name="startDate" value={formik.values.startDate} onChange={formik.handleChange} className="form-input" size="45"></input>
+              </p>
+              <p>
+                <label htmlFor="endDate">End Date: </label>
+                <input type="date" name="endDate" value={formik.values.endDate} onChange={formik.handleChange} className="form-input" size="45"></input>
+              </p>
+            </div>
           </form>
         </div>
       );
